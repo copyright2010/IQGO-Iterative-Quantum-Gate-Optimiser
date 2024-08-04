7de5c349f065bfb0752332f6d8293a0cf0dbf771
t ...... <t.lisas2010@gmail.com>
2024-08-04 17:19:34 +0100

    Model Initialisation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='iqgo',
+    name='IQGO_module',
     version='0.1.0',
     author='Your Name',
     author_email='d17125255@mytudublin.ie',

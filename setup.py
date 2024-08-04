4afb234e995bec4547c29f11fe9ced3f85258a2a
t ...... <t.lisas2010@gmail.com>
2024-08-04 17:24:42 +0100

    Model Initialisation

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='IQGO_module',
     version='0.1.0',
-    author='Your Name',
+    author='Tautvydas Lisas',
     author_email='d17125255@mytudublin.ie',
     description='IQGO: Iterative Quantum Gate Optimiser',
     long_description=open('README.md').read(),
     long_description_content_type='Classification Algorithm',
-    url='https://github.com/copyright2010/IQGO-Iterative-Quantum-Gate-Optimiser',  # Replace with your repository URL
+    url='https://github.com/copyright2010/IQGO-Iterative-Quantum-Gate-Optimiser', 
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
